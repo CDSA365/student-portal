ee21ba308d2b1a0d27a43b28e39c6583654cad6c
samnsimson <samnishanth01@gmail.com>
2022-11-13 17:27:57 +0530

    memoizing routes to prevent re-renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Spin } from "antd";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useMemo, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
 import NotFoundPage from "./pages/_404/_404";
@@ -14,13 +14,8 @@ const SuspenseFallback = () => {
 };
 
 function App() {
-    const [publicRoutes, setPublicRoutes] = useState<any[]>([]);
-    const [privateRoutes, setPrivateRoutes] = useState<any[]>([]);
-
-    useEffect(() => {
-        setPublicRoutes(routes.filter((x) => x.public));
-        setPrivateRoutes(routes.filter((x) => !x.public));
-    }, []);
+    const publicRoutes = useMemo(() => routes.filter((x) => x.public), [routes]);
+    const privateRoutes = useMemo(() => routes.filter((x) => !x.public), [routes]);
 
     return (
         <BrowserRouter>
@@ -28,16 +23,9 @@ function App() {
                 <Routes>
                     <Route element={<Dashboard />}>
                         {privateRoutes.map(({ component: Component, path }) => (
-                            <Route
-                                path={path}
-                                element={<Component />}
-                                key={path}
-                            />
+                            <Route path={path} element={<Component />} key={path} />
                         ))}
-                        <Route
-                            path="/page-not-found"
-                            element={<NotFoundPage />}
-                        />
+                        <Route path="/page-not-found" element={<NotFoundPage />} />
                         <Route path="*" element={<NotFoundPage />} />
                     </Route>
                     {publicRoutes.map(({ component: Component, path }) => (
